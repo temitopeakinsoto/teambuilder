08b694aa0cebb2c39d868cad960e90a576c02324
Temitope Akinsoto <takinsoto@gmail.com>
2019-11-27 19:39:40 +0100

    Import <Form/> and <Memeber/> components to top-level <App /> component

--- teambuilder/src/App.js
+++ teambuilder/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Form from './components/Form';
+import Member from './components/Member';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome, REACT</h1>
+      <Form />
+      <Member />
     </div>
   );
 }
---
+++ teambuilder/src/components/Member.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Member() {
+    return (
+        <div>
+            <p>This is our member component</p>
+        </div>
+    )
+}
