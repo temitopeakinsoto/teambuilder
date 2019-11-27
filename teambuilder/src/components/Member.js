ebeb100b077481bd447577ce73bcb12010d178ed
Temitope Akinsoto <takinsoto@gmail.com>
2019-11-27 20:45:30 +0100

    Modify <Member/> component
    In this commit, I display the fullname, email and role of each member.
    These values were recieved from the props object which was passed from th app component

--- teambuilder/src/components/Member.js
+++ teambuilder/src/components/Member.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-export default function Member() {
+export default function Member(props) {
+    const { email, fullname, role } = props;
     return (
         <div>
-            <p>This is our member component</p>
+            <p>NAME: {fullname}, EMAIL: {email}, ROLE: {role}</p>
         </div>
     )
 }
