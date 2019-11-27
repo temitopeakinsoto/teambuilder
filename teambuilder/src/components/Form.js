2dd9fadb6aa43fd24854371eb2c61ef2d56cc3a4
Temitope Akinsoto <takinsoto@gmail.com>
2019-11-27 20:43:10 +0100

    Add form input fields
    In this commit, I added form input fields. An input field for fullname, email and a select field for role

--- teambuilder/src/components/Form.js
+++ teambuilder/src/components/Form.js
@@ -1,10 +1,27 @@
 import React from 'react'
 
-export default function Form() {
+export default function Form(props) {
     return (
-        <div>
-            This is our form component
-        </div>
+        <form>
+            <label htmlFor="fullnameInput">Name</label>
+            <input
+            id="fullnameInput"
+            name="fullname"
+            value={""}
+            /><br/>
+            <label htmlFor="emailInput">Email</label>
+            <input
+            id="emailInput"
+            name="email"
+            value={""}
+            /><br/>
+            <select> 
+                <option></option>
+                <option>FRONT END</option>
+                <option>BACK END</option>
+                <option>FULL STACK</option>
+            </select>
+        </form>
     )
 }
 
