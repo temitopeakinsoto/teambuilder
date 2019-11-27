48ae901a7fa3b9754e0256eaf6341bfdc36cff2d
Temitope Akinsoto <takinsoto@gmail.com>
2019-11-27 20:55:23 +0100

    Initialize application state
    In this commit, I created two application states: membersList and newMember
    The membersList is for holding the list of members while the newMeber is for storing the input fields from form component

--- teambuilder/src/App.js
+++ teambuilder/src/App.js
@@ -1,15 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
+import uuid from 'uuid';
 import Form from './components/Form';
 import Member from './components/Member';
 
 import './App.css';
 
+const initialMembersList = [
+  {
+   id: uuid(),
+   fullname: "Temitope Akinsoto",
+   email: "temitopeakinsoto@lambdaschool.com",
+   role: "Full Stack Developer" 
+  },
+  {
+    id: uuid(),
+    fullname: "Justin Irabor",
+    email: "justinirabor@lambdaschool.com",
+    role: "Front End Developer" 
+   },
+   {
+    id: uuid(),
+    fullname: "Oluwafemi Anjorin",
+    email: "oluwafemianjorin@lambdaschool.com",
+    role: "Full Stack Developer" 
+   },
+   {
+    id: uuid(),
+    fullname: "David Kuseh",
+    email: "davidkuseh@lambdaschool.com",
+    role: "Full Stack Developer" 
+   },
+   {
+    id: uuid(),
+    fullname: "Victor Arowo",
+    email: "victorarowo@lambdaschool.com",
+    role: "Full Stack Developer" 
+   },
+]
+
+const memberFormInput = {
+  fullname: "",
+  email: "",
+  role: ""
+}
+
 function App() {
+  const [membersList, setMembersList] = useState(initialMembersList);
+  const [newMember, setNewMember] = useState(memberFormInput)
   return (
     <div className="App">
       <h1>Welcome, REACT</h1>
       <Form />
-      <Member />
+      {
+        membersList.map(member => (
+          <Member  
+          id={member.id} 
+          fullname={member.fullname}
+          email={member.email}
+          role={member.role}
+          />
+        ))
+      }
+
+      
+      
     </div>
   );
 }
